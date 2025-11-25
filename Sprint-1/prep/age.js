a22a96da6853a2ff6c11968bf80f4b5ccc560770
Nikolai Romanov <kolya.rm@gmail.com>
2025-11-25 11:16:22 +0100

    Added prep directories in module and sprint-1 directories. In sprint-1 directory added example javascript file.

---
+++ Sprint-1/prep/age.js
@@ -0,0 +1,6 @@
+const yearOfBirth = 1990; //declaration
+const currentYear = 2025; //declaration
+
+// currentYear++; //statement
+
+console.log(`I'm ${currentYear - yearOfBirth} years old`); //statement
