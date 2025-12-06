6bcbe7f1f17e2a33c57fc75b584a5672a7f06be5
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-06 16:55:17 +0100

    Assertion.js file which contains assertion example added in sprint 2 prep folder.

---
+++ Sprint-2/prep/asertions.js
@@ -0,0 +1,10 @@
+function formatAs12HourClock(time) {
+  return `${time} am`;
+}
+
+const currentOutput = formatAs12HourClock("08:00");
+const targetOutput = "08:00 am";
+console.assert(
+  currentOutput === targetOutput,
+  `current output: ${currentOutput}, target output: ${targetOutput} `
+);
