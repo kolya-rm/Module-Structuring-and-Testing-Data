295230ce6c4d2a71943e20db5c83be6827bd864b
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-07 21:47:06 +0100

    Added tests for input groups in sprint 2 prep assertions.js.

--- Sprint-2/prep/assertions.js
+++ Sprint-2/prep/assertions.js
@@ -10,19 +10,47 @@ const currentOutput = formatAs12HourClock("08:00");
 const targetOutput = "08:00 am";
 console.assert(
   currentOutput === targetOutput,
-  `current output: ${currentOutput}, target output: ${targetOutput} `
+  `current output: ${currentOutput}, target output: ${targetOutput}`
 );
 
 const currentOutput1 = formatAs12HourClock("23:00");
 const targetOutput1 = "11:00 pm";
 console.assert(
   currentOutput1 === targetOutput1,
-  `current output: ${currentOutput1}, target output: ${targetOutput1} `
+  `current output: ${currentOutput1}, target output: ${targetOutput1}`
 );
 
 const currentOutput2 = formatAs12HourClock("16:00");
 const targetOutput2 = "04:00 pm";
 console.assert(
   currentOutput2 === targetOutput2,
-  `current output: ${currentOutput2}, target output: ${targetOutput2} `
+  `current output: ${currentOutput2}, target output: ${targetOutput2}`
+);
+
+const currentOutput3 = formatAs12HourClock("16:08");
+const targetOutput3 = "04:08 pm";
+console.assert(
+  currentOutput3 === targetOutput3,
+  `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("16:30");
+const targetOutput4 = "04:30 pm";
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+const currentOutput5 = formatAs12HourClock("08:08");
+const targetOutput5 = "08:08 am";
+console.assert(
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
+);
+
+const currentOutput6 = formatAs12HourClock("08:38");
+const targetOutput6 = "08:38 am";
+console.assert(
+  currentOutput6 === targetOutput6,
+  `current output: ${currentOutput6}, target output: ${targetOutput6}`
 );
