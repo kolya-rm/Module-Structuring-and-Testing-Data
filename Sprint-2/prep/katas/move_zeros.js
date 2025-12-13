1fb284751e1a4c7926bdaa53ad06c278a0a5c789
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-13 13:28:46 +0100

    Added move zeroes kata solution.

--- Sprint-2/prep/katas/move_zeros.js
+++ Sprint-2/prep/katas/move_zeros.js
@@ -1,11 +1,36 @@
 function moveZeros(array) {
-  let result = '';
+  let nonZerosArray = [];
+  let zerosArray = [];
   for (let i = 0; i < array.length; i++) {
-    if (array[i] != 0) {
-      result += array[i];
+    if (array[i] === 0) {
+      zerosArray.push(array[i]);
+    } else {
+      nonZerosArray.push(array[i]);
     }
   }
-  return result.padEnd(array.length, '0').split('').map(digit => Number(digit));
+  return [...nonZerosArray, ...zerosArray];
 }
 
-console.log(moveZeros([0, 1, 0, 2, 0]));
+// Requirement: Moves zeros to the end
+console.assert(
+    (moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])) === ([1, 2, 1, 1, 3, 1, 0, 0, 0, 0]),
+    "Failed: Should move zeros to end while keeping order"
+);
+
+// Requirement: Does nothing if no zeros
+console.assert(
+    (moveZeros([1, 2, 3])) === ([1, 2, 3]),
+    "Failed: Should not change array with no zeros"
+);
+
+// Requirement: Handles all zeros
+console.assert(
+    (moveZeros([0, 0, 0])) === ([0, 0, 0]),
+    "Failed: Should handle array of all zeros"
+);
+
+// Requirement: Handles mixed types (false is not 0)
+console.assert(
+    (moveZeros([false, 1, 0, 1, 2, 0, "a"])) === ([false, 1, 1, 2, "a", 0, 0]),
+    "Failed: Should treat 'false' or strings differently than the number 0"
+);
