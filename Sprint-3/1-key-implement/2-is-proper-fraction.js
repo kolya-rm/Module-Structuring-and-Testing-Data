bb25404eb9f801ca1555521273bf2773abb82ebc
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-17 00:09:35 +0100

    Update the function logic in 2-is-proper-fraction.js sprint 3 key implement part.

--- Sprint-3/1-key-implement/2-is-proper-fraction.js
+++ Sprint-3/1-key-implement/2-is-proper-fraction.js
@@ -8,11 +8,7 @@
 // write one test at a time, and make it pass, build your solution up methodically
 
 function isProperFraction(numerator, denominator) {
-    if (Math.abs(numerator) < Math.abs(denominator)) {
-      return true;
-    } else {
-      return false;
-    }
+    return Math.abs(numerator / denominator) < 1;
 }
 
 // here's our helper again
