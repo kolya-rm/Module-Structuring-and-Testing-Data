a61f5846ef83ff75c58458b22488b98ed4370766
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-18 21:38:38 +0100

    Implemented tests for Number cards for the getCardValue(card) function in the sprint 3 mandatory rewrite part.

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,45 @@
 const getCardValue = require("./3-get-card-value");
 
-test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
+test("Should return 11 for Ace of Spades", () => {
+    const aceOfSpades = getCardValue("A♠");
+    expect(aceOfSpades).toEqual(11);
     });
 
 // Case 2: Handle Number Cards (2-10):
+test("Should return 2 for Two of Hearts", () => {
+    const twoOfHearts = "2♥";
+    expect(getCardValue(twoOfHearts)).toEqual(2);
+});
+
+test("Should return 3 for Three of Hearts", () => {
+  const threeOfHearts = "3♥";
+  expect(getCardValue(threeOfHearts)).toEqual(3);
+});
+
+test("Should return 4 for Four of Hearts", () => {
+  const fourOfHearts = "4♥";
+  expect(getCardValue(fourOfHearts)).toEqual(4);
+});
+
+test("Should return 5 for Five of Hearts", () => {
+  const fiveOfHearts = "5♥";
+  expect(getCardValue(fiveOfHearts)).toEqual(5);
+});
+
+test("Should return 6 for Six of Hearts", () => {
+  const sixOfHearts = "6♥";
+  expect(getCardValue(sixOfHearts)).toEqual(6);
+});
+
+test("Should return 7 for Seven of Hearts", () => {
+  const sevenOfHearts = "7♥";
+  expect(getCardValue(sevenOfHearts)).toEqual(7);
+});
+
+test("Should return 8 for Eight of Hearts", () => {
+  const eightOfHearts = "8♥";
+  expect(getCardValue(eightOfHearts)).toEqual(8);
+});
 // Case 3: Handle Face Cards (J, Q, K):
 // Case 4: Handle Ace (A):
 // Case 5: Handle Invalid Cards:
