c82c1fe1a55c7e44ca091f45972144ae6066ffe5
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-18 10:24:16 +0100

    Implemented a test for getAngleType(angle) function for the case of obtuse angles in sprint 3 mandatory rewrite section.

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -7,13 +7,13 @@ test("should identify right angle (90°)", () => {
 // REPLACE the comments with the tests
 // make your test descriptions as clear and readable as possible
 
-test("Should identified acute angles (<90°)", () => {
+test("Should identified acute angles (angle < 90°)", () => {
   expect(getAngleType(45)).toEqual("Acute angle");
 });
 
-// Case 3: Identify Obtuse Angles:
-// When the angle is greater than 90 degrees and less than 180 degrees,
-// Then the function should return "Obtuse angle"
+test("Should identified obtuse angles (90° < angle < 180°)", () => {
+  expect(getAngleType(120)).toEqual("Obtuse angle");
+});
 
 // Case 4: Identify Straight Angles:
 // When the angle is exactly 180 degrees,
