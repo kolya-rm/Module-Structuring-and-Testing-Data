8b8cc2aa733a64720b81896a99d31d131bdbc2df
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-18 10:36:31 +0100

    Implemented a test for getAngleType(angle) function for the case of straight angle in sprint 3 mandatory rewrite section.

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -1,23 +1,23 @@
 const getAngleType = require("./1-get-angle-type");
 
-test("should identify right angle (90°)", () => {
+test("should identify right angle (angle = 90°)", () => {
   expect(getAngleType(90)).toEqual("Right angle");
 });
 
 // REPLACE the comments with the tests
 // make your test descriptions as clear and readable as possible
 
-test("Should identified acute angles (angle < 90°)", () => {
+test("Should identify acute angles (angle < 90°)", () => {
   expect(getAngleType(45)).toEqual("Acute angle");
 });
 
-test("Should identified obtuse angles (90° < angle < 180°)", () => {
+test("Should identify obtuse angles (90° < angle < 180°)", () => {
   expect(getAngleType(120)).toEqual("Obtuse angle");
 });
 
-// Case 4: Identify Straight Angles:
-// When the angle is exactly 180 degrees,
-// Then the function should return "Straight angle"
+test("Should identify straight angle (angle = 180°)", () => {
+  expect(getAngleType(180)).toEqual("Straight angle");
+});
 
 // Case 5: Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
