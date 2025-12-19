6c1ba583c58b14c77e765a94742fc1decde0ccc3
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-19 11:45:19 +0100

    Added a test for the countChar(string, char) function in case of the char doesn't exists in the string in sprint 3 mandatory practice / implement part.

--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -22,3 +22,10 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+test("", () => {
+  const str = "AAAAA";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
