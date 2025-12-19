dca6fd25272708338c3ab16fcfbe8ffc5e429253
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-19 12:09:49 +0100

    Added a test for the repeat(str, count) function in case of repeat count is 1 in sprint 3 mandatory practice / implement part.

--- Sprint-3/3-mandatory-practice/implement/repeat.test.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.test.js
@@ -21,6 +21,13 @@ test("should repeat the string count times", () => {
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
 
+test("Should return the original string", () => {
+    const str = "hello";
+    const count = 1;
+    const repeatedString = repeat(str, count);
+    expect(repeatedString).toEqual(str);
+});
+
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
