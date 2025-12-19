d517b6189110783552a9388c6afb00503fc22590
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-19 21:15:11 +0100

    Added tests for the getOrdinalNumber(num) function for numbers ending in 2 in sprint 3 mandatory practice/implement part.

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -4,7 +4,8 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
 
-// Case 1: Identify the ordinal numbers ending in 1 except ending in 11
+// Case 1: Identify the ordinal for numbers ending in 1. Should return ordinals ending in '1st'
+// except numbers ending in 11 for which should return ordinals ending in '11th'
 
 test("should return '1st' for 1", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
@@ -28,4 +29,31 @@ test("Should return '111th' for '111'", () => {
 
 test("should return '121st' for 121", () => {
   expect(getOrdinalNumber(121)).toEqual("121st");
+});
+
+// Case 2: Identify the ordinal for numbers ending in 2. Should return ordinals ending in '2nd'
+// except numbers ending in 12 for which should return ordinals ending in '12th'
+
+test("should return '2nd' for 2", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+});
+
+test("Should return '12th' for '12'", () => {
+  expect(getOrdinalNumber(12)).toEqual("12th");
+});
+
+test("should return '22nd' for 22", () => {
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+});
+
+test("Should return '102nd' for '102'", () => {
+  expect(getOrdinalNumber(101)).toEqual("102nd");
+});
+
+test("Should return '112th' for '112'", () => {
+  expect(getOrdinalNumber(112)).toEqual("112th");
+});
+
+test("should return '122nd' for 122", () => {
+  expect(getOrdinalNumber(121)).toEqual("122nd");
 });
