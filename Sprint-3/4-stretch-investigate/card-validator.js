a99ff2a381f663f116b5caf542eda33bd13c37a8
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-20 22:21:08 +0100

    Commented logic of the cardValidator(cardNumber) function in sprint 3 stretch investigate part.

--- Sprint-3/4-stretch-investigate/card-validator.js
+++ Sprint-3/4-stretch-investigate/card-validator.js
@@ -1,4 +1,5 @@
 function cardValidator(cardNumber) {
+  //checking the card number length
   if (cardNumber.length !== 16) {
     return false;
   }
@@ -6,6 +7,7 @@ function cardValidator(cardNumber) {
   const digitsRegEx = /\d/;
   const lastChar = cardNumber[cardNumber.length - 1];
 
+  //checking the card number last character is even digit
   if (digitsRegEx.test(lastChar) && Number(lastChar) % 2 != 0) {
     return false;
   }
@@ -14,15 +16,20 @@ function cardValidator(cardNumber) {
   
   for (let i = 0; i < cardNumber.length; i++) {
     const currentChar = cardNumber[i];
+    //checking all the card number characters are digits
     if (!digitsRegEx.test(currentChar)) {
       return false;
     }
+    //summing digits of the card number
     digitsSum += Number(currentChar);
   }
+  //checking sum of digits of the card number
   if (digitsSum < 16) {
     return false;
   }
+  //returning true if all checks passed
   return true;
 }
+
 
 module.exports = cardValidator;
