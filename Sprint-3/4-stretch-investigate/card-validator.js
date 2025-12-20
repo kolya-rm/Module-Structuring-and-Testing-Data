6942ad149fa4b50f239e589e75e1a48be4d7ab0b
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-20 21:37:03 +0100

    For the card validator project in sprint 3 stretch investigate part:   - implemented checking the card number contains non-digit characters logic;   - added the test checks the card number contains non-digit characters;

--- Sprint-3/4-stretch-investigate/card-validator.js
+++ Sprint-3/4-stretch-investigate/card-validator.js
@@ -1,6 +1,14 @@
 function cardValidator(cardNumber) {
+  const digitsRegEx = /\d/;
+
   if (cardNumber.length !== 16) {
     return false;
+  }
+  for (let i = 0; i < cardNumber.length - 1; i++) {
+    const currentChar = cardNumber[i];
+    if (!digitsRegEx.test(cardNumber)) {
+      return false;
+    }
   }
 }
 
--- Sprint-3/4-stretch-investigate/card-validator.test.js
+++ Sprint-3/4-stretch-investigate/card-validator.test.js
@@ -1,8 +1,15 @@
 const isValidCard = require("./card-validator");
 
 test("Card number should be 16 characters length.", () => {
-  const cardNumber = "123412341234123";
+  const cardNumber = "0000111122223333444";
   const targetOutput = isValidCard(cardNumber);
 
   expect(targetOutput).toBe(false);
 })
+
+test("Card number should not contains non-digit characters", () => {
+  const cardNumber = "0000111122223333444a";
+  const targetOutput = isValidCard(cardNumber);
+
+  expect(targetOutput).toBe(false);
+});
