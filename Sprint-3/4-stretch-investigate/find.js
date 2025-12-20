8b9783398c8d3652e253a2cf4f0aea2c7791ab23
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-20 18:29:50 +0100

    Added answers in the find.js file in sprint 3 stretch investigate part.

--- Sprint-3/4-stretch-investigate/find.js
+++ Sprint-3/4-stretch-investigate/find.js
@@ -20,6 +20,14 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+// In the begin of function after the declaration the index variable initialize with value 0.
+// Then in the end of the every iteration of the while loop it increased by 1.
+
 // b) What is the if statement used to check
+// The if statement checks the value of the index variable exceeds the string last character iindex.
+
 // c) Why is index++ being used?
+// Because we the while loop without a counter instead of a for loop with the known count of iteration.
+
 // d) What is the condition index < str.length used for?
+// This condition indicate reaching of the end of the given string and exiting from the while loop.
