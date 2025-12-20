f050c136a8f71d631a556d56d5856574565b36e2
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-20 21:53:43 +0100

    For the card validator project in sprint 3 stretch investigate part:   - implemented checking the card number digit sum is not less tah 16;   - added the test checks the card number digits sum is not less than 16;   - fixed logic for the check card number contains only digits;   - updated testing card nuber values.

--- Sprint-3/4-stretch-investigate/card-validator.js
+++ Sprint-3/4-stretch-investigate/card-validator.js
@@ -1,16 +1,20 @@
 function cardValidator(cardNumber) {
   const digitsRegEx = /\d/;
+  let digitsSum = 0;
 
   if (cardNumber.length !== 16) {
     return false;
   }
-  for (let i = 0; i < cardNumber.length - 1; i++) {
+  for (let i = 0; i < cardNumber.length; i++) {
     const currentChar = cardNumber[i];
-    if (!digitsRegEx.test(cardNumber)) {
+    if (!digitsRegEx.test(currentChar)) {
       return false;
     }
+    digitsSum += Number(currentChar);
+  }
+  if (digitsSum < 16) {
+    return false;
   }
 }
-
 
 module.exports = cardValidator;
--- Sprint-3/4-stretch-investigate/card-validator.test.js
+++ Sprint-3/4-stretch-investigate/card-validator.test.js
@@ -1,15 +1,22 @@
 const isValidCard = require("./card-validator");
 
 test("Card number should be 16 characters length.", () => {
-  const cardNumber = "0000111122223333444";
+  const cardNumber = "000011112222333";
   const targetOutput = isValidCard(cardNumber);
 
   expect(targetOutput).toBe(false);
 })
 
 test("Card number should not contains non-digit characters", () => {
-  const cardNumber = "0000111122223333444a";
+  const cardNumber = "000011112222333a";
   const targetOutput = isValidCard(cardNumber);
 
   expect(targetOutput).toBe(false);
 });
+
+test("Sum of the card number digits should not be less than 16", () => {
+  const cardNumber = "0000111100001111";
+  const targetOutput = isValidCard(cardNumber);
+
+  expect(targetOutput).toBe(false);
+})
