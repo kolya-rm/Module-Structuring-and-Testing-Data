9dd6049577d03918ca1ea026e6d4dc59197db0ed
Nikolai Romanov <kolya.rm@gmail.com>
2025-12-20 22:06:57 +0100

    For the card validator project in sprint 3 stretch investigate part:   - implemented checking the card number ends in an even digit;   - added the test checks the card number ends in an even digit;   - updated testing card number values.

--- Sprint-3/4-stretch-investigate/card-validator.js
+++ Sprint-3/4-stretch-investigate/card-validator.js
@@ -1,10 +1,17 @@
 function cardValidator(cardNumber) {
-  const digitsRegEx = /\d/;
-  let digitsSum = 0;
-
   if (cardNumber.length !== 16) {
     return false;
   }
+
+  const digitsRegEx = /\d/;
+  const lastChar = cardNumber[cardNumber - 1];
+
+  if (digitsRegEx.test(lastChar) && Number(lastChar) % 2 != 0) {
+    return false;
+  }
+
+  let digitsSum = 0;
+  
   for (let i = 0; i < cardNumber.length; i++) {
     const currentChar = cardNumber[i];
     if (!digitsRegEx.test(currentChar)) {
--- Sprint-3/4-stretch-investigate/card-validator.test.js
+++ Sprint-3/4-stretch-investigate/card-validator.test.js
@@ -1,21 +1,21 @@
 const isValidCard = require("./card-validator");
 
 test("Card number should be 16 characters length.", () => {
-  const cardNumber = "000011112222333";
+  const cardNumber = "000044440000222";
   const targetOutput = isValidCard(cardNumber);
 
   expect(targetOutput).toBe(false);
 })
 
 test("Card number should not contains non-digit characters", () => {
-  const cardNumber = "000011112222333a";
+  const cardNumber = "000044440000a222";
   const targetOutput = isValidCard(cardNumber);
 
   expect(targetOutput).toBe(false);
 });
 
 test("Sum of the card number digits should not be less than 16", () => {
-  const cardNumber = "0000111100001111";
+  const cardNumber = "0000111100002222";
   const targetOutput = isValidCard(cardNumber);
 
   expect(targetOutput).toBe(false);
